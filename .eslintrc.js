d4445fe9b8096e7b75a8698ca8e14970ff2f75b6
Soab42 <108150913+Soab42@users.noreply.github.com>
2023-03-10 22:22:06 +0600

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,60 +5,32 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'airbnb-base',
-    'prettier',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb-base",
+    "prettier",
   ],
-  overrides: [
-    // override "simple-import-sort" config
-    {
-      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
-      rules: {
-        "simple-import-sort/imports": [
-          "error",
-          {
-            groups: [
-              // Packages `react` related packages come first.
-              ["^react", "^@?\\w"],
-              // Internal packages.
-              ["^(@|components)(/.*|$)"],
-              // Side effect imports.
-              ["^\\u0000"],
-              // Parent imports. Put `..` last.
-              ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
-              // Other relative imports. Put same-folder imports and `.` last.
-              ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
-              // Style imports.
-              ["^.+\\.?(css)$"],
-            ],
-          },
-        ],
-      },
-    },
-  ],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['react', 'prettier', 'simple-import-sort'],
+  plugins: ["react", "prettier", "simple-import-sort"],
   rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
-    quotes: ['error', 'auto'],
-    semi: ['error', 'always'],
-    'prettier/prettier': [
-      'error',
+    indent: ["error", 2],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "prettier/prettier": [
+      "error",
       {
-        endOfLine: 'auto',
-        trailingComma: 'es5',
+        endOfLine: "auto",
+        trailingComma: "es5",
         tabWidth: 2,
         semi: true,
-        singleQuote: true,
+        singleQuote: false,
         // parser: 'flow',
       },
     ],
-    "simple-import-sort/imports": "error",/
-    "simple-import-sort/exports": "error",
   },
 };
